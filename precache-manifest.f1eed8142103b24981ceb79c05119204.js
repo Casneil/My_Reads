3de69146bc571a8518366a9b212d22d1601bdfc0
Casneil Simpson <casneil.simpson@gmail.com>
2019-10-20 08:24:47 +0200

    Updates

---
+++ precache-manifest.f1eed8142103b24981ceb79c05119204.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/My_Reads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/My_Reads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/My_Reads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "10bceda04a0eb1a01b63",
+    "url": "/My_Reads/static/js/runtime~main.10bceda0.js"
+  },
+  {
+    "revision": "9a3ea0ccac64541cfdb4",
+    "url": "/My_Reads/static/js/main.9a3ea0cc.chunk.js"
+  },
+  {
+    "revision": "7ed97d6fdd64d9f0454f",
+    "url": "/My_Reads/static/js/1.7ed97d6f.chunk.js"
+  },
+  {
+    "revision": "9a3ea0ccac64541cfdb4",
+    "url": "/My_Reads/static/css/main.29d19c5b.chunk.css"
+  },
+  {
+    "revision": "4960fc7375c8e32b2b6b7942b00635dd",
+    "url": "/My_Reads/index.html"
+  }
+];
